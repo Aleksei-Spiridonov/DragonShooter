d368cf9aba914fc425e9958cd986554104019ffc
Aleksei <leha.alexjazzy.spiridonov@gmail.com>
2025-12-11 12:32:02 -0500

    Features :)

---
+++ Source/GEngine3Aleksei/Enemy/Dragon.cpp
@@ -0,0 +1,527 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Dragon.h"
+
+#include "Components/CapsuleComponent.h"
+#include "PhysicsControlComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "DrawDebugHelpers.h"
+
+ADragon::ADragon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>("DragonMesh");
+	SetRootComponent(SkeletalMeshComponent);
+	SkeletalMeshComponent->SetSimulatePhysics(true);
+
+	SkeletalMeshComponent->SetEnableGravity(false);
+	SkeletalMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	PhysicsControl = CreateDefaultSubobject<UPhysicsControlComponent>("PhysicsControl");
+
+	// Create 4 attach points for legs
+	for (int i = 0; i < 4; i++)
+	{
+		USceneComponent* NewAttachPoint = CreateDefaultSubobject<USceneComponent>(
+			*FString::Printf(TEXT("AttachPoint%d"), i));
+		NewAttachPoint->SetupAttachment(RootComponent);
+		AttachPoints.Add(NewAttachPoint);
+	}
+}
+
+void ADragon::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                    FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!bIsAttached)
+	{
+		reattach();
+
+		if (!bIsAttached)
+		{
+			force += Hit.Normal * 40;
+
+			FVector impactNormal = Hit.Normal.GetSafeNormal();
+
+			// Direction from actor to hit point
+			FVector relativeHitPoint = (Hit.ImpactPoint - SkeletalMeshComponent->GetComponentLocation()).
+				GetSafeNormal();
+
+			FVector rotationAxis = FVector::CrossProduct(relativeHitPoint, impactNormal);
+
+			if (rotationAxis.IsNearlyZero())
+			{
+				rotationAxis = impactNormal;
+			}
+
+			rotationAxis.Normalize();
+			float rotationAngle = NormalImpulse.Size() * 0.00002f;
+			FQuat smallRotation = FQuat(rotationAxis, rotationAngle);
+
+			targetRotation = smallRotation * targetRotation;
+		}
+	}
+}
+
+void ADragon::AddTranslation(FVector DeltaPos)
+{
+	SkeletalMeshComponent->SetRelativeLocation(SkeletalMeshComponent->GetRelativeLocation() + DeltaPos, false, nullptr,
+	                                           ETeleportType::TeleportPhysics);
+}
+
+
+void ADragon::SetTranslation(FVector Pos)
+{
+	SkeletalMeshComponent->SetRelativeLocation(Pos, false, nullptr, ETeleportType::TeleportPhysics);
+}
+
+
+void ADragon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	targetRotation = SkeletalMeshComponent->GetComponentQuat();
+	SkeletalMeshComponent->OnComponentHit.AddDynamic(this, &ADragon::OnHit);
+
+	float forceBase = 30000;
+
+	FPhysicsControlData controlData = FPhysicsControlData();
+	controlData.LinearStrength = forceBase;
+	controlData.LinearDampingRatio = 1;
+	controlData.MaxForce = forceBase;
+
+	controlData.AngularStrength = forceBase * 0.1;
+	controlData.AngularDampingRatio = 1;
+	controlData.MaxTorque = forceBase * 0.1;
+
+	controlData.bUseSkeletalAnimation = true;
+
+	TArray<FName> names = PhysicsControl->CreateControlsFromSkeletalMeshBelow(
+		SkeletalMeshComponent, SkeletalMeshComponent->GetBoneName(0), true, EPhysicsControlType::WorldSpace,
+		controlData,
+		TEXT("All"));
+
+
+	if (defaultAttachPositions.Num() != 4)
+	{
+		defaultAttachPositions.Empty();
+		defaultLegCenter = FVector(0, 0, 0);
+		/*
+		for (FName legBoneName : legNames)
+		{
+			FRigidBodyState legState;
+			SkeletalMeshComponent->GetRigidBodyState(legState, legBoneName);
+			FVector legPosWorld = legState.Position;
+			FVector legPosLocal = SkeletalMeshComponent->GetComponentTransform().InverseTransformPosition(legPosWorld);
+			
+			defaultAttachPositions.Add(legPosLocal);
+			defaultLegCenter += legPosLocal;
+		}
+		defaultLegCenter /= legNames.Num();
+		*/
+
+		defaultAttachPositions = {
+			FVector(20, 62.428681, 0),
+			FVector(20, -30.550485, 0),
+			FVector(-20, 62.428681, 0),
+			FVector(-20, -30.550485, 0),
+		};
+		defaultLegCenter = {};
+		for (int i = 0; i < 4; i++)
+		{
+			defaultLegCenter += defaultAttachPositions[i] / 4;
+		}
+	}
+}
+
+void ADragon::reattach()
+{
+	const FVector FirstRayLocalOrigin = FVector(0.f, 0.f, 100.f);
+	const int NumCenterRays = 16;
+	const float ConeAngleDeg = 30.f;
+	const float FirstRayDistance = 200.f;
+	const int MinFirstHits = 6; 
+	const float PlaneFitMaxRMS = 10.0f; 
+	const float AlignDotThreshold = 0.7f;
+	const float MinLegSurfaceDot = 0.71f;
+
+	// Helper: world-space origin for first rays
+	const FTransform ActorTransform = GetActorTransform();
+	const FVector FirstRayWorldOrigin = ActorTransform.TransformPosition(FirstRayLocalOrigin);
+
+	// Compute the cone axis: local down in world space
+	FVector LocalDownWorld = ActorTransform.TransformVector(FVector(0.f, 0.f, -1.f));
+	LocalDownWorld.Normalize();
+
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	// First raycast: cone of rays from FirstRayWorldOrigin toward local down
+	TArray<FVector> firstHitPositions;
+	firstHitPositions.Reserve(NumCenterRays);
+
+	// Create directions evenly around azimuth on fixed cone angle
+	const float ConeRad = FMath::DegreesToRadians(ConeAngleDeg);
+	for (int i = 0; i < NumCenterRays; ++i)
+	{
+		// Azimuth evenly distributed
+		float Azimuth = (2.f * PI) * (float(i) / float(NumCenterRays));
+		FVector Perp = LocalDownWorld.GetAbs().X < 0.99f
+			               ? FVector::CrossProduct(LocalDownWorld, FVector::RightVector)
+			               : FVector::CrossProduct(LocalDownWorld, FVector::UpVector);
+		Perp.Normalize();
+
+		FQuat TiltQuat = FQuat(Perp, ConeRad); // tilt away from axis by cone angle
+		FQuat SpinQuat = FQuat(LocalDownWorld, Azimuth); // spin around axis
+		FVector Dir = SpinQuat * (TiltQuat * LocalDownWorld); // final direction
+		Dir.Normalize();
+
+		FVector RayStart = FirstRayWorldOrigin;
+		FVector RayEnd = RayStart + Dir * FirstRayDistance;
+
+		FHitResult Hit;
+		bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, RayStart, RayEnd, ECollisionChannel::ECC_Visibility,
+		                                                 Params);
+		if (bHit)
+		{
+			firstHitPositions.Add(Hit.ImpactPoint);
+		}
+	}
+
+	// Need at least MinFirstHits hits
+	if (firstHitPositions.Num() < MinFirstHits)
+	{
+		bIsAttached = false;
+		return;
+	}
+
+	// Fit a plane to the hit positions
+	const int PN = firstHitPositions.Num();
+	// Find pair A,B with maximum distance
+	int idxA = 0, idxB = 1;
+	float maxPairDistSq = 0.f;
+	for (int i = 0; i < PN; i++)
+	{
+		for (int j = i + 1; j < PN; j++)
+		{
+			float d2 = FVector::DistSquared(firstHitPositions[i], firstHitPositions[j]);
+			if (d2 > maxPairDistSq)
+			{
+				maxPairDistSq = d2;
+				idxA = i;
+				idxB = j;
+			}
+		}
+	}
+
+	// Find C maximizing distance to line AB
+	FVector PA = firstHitPositions[idxA];
+	FVector PB = firstHitPositions[idxB];
+	FVector AB = PB - PA;
+	float ABLen = AB.Size();
+	if (ABLen < 0.0001)
+	{
+		bIsAttached = false;
+		return;
+	}
+	int idxC = -1;
+	float maxDistToLine = -1.f;
+	for (int i = 0; i < PN; ++i)
+	{
+		// distance from point to line  AB: |(AP x AB)| / |AB|
+		FVector AP = firstHitPositions[i] - PA;
+		FVector Cross = FVector::CrossProduct(AP, AB);
+		float distToLine = Cross.Size() / ABLen;
+		if (distToLine > maxDistToLine)
+		{
+			maxDistToLine = distToLine;
+			idxC = i;
+		}
+	}
+
+	if (idxC == -1)
+	{
+		bIsAttached = false;
+		return;
+	}
+
+	FVector PC = firstHitPositions[idxC];
+	FVector Normal = FVector::CrossProduct(AB, PC - PA);
+	if (Normal.IsNearlyZero())
+	{
+		bIsAttached = false;
+		return;
+	}
+	Normal.Normalize();
+
+	// plane RMS error (distance of each point to the plane defined by (PA, Normal))
+	double sumSq = 0.0;
+	for (const FVector& P : firstHitPositions)
+	{
+		double dist = FMath::Abs(FVector::DotProduct(Normal, (P - PA)));
+		sumSq += dist * dist;
+	}
+	double rms = FMath::Sqrt(sumSq / (double)PN);
+
+	if (rms > PlaneFitMaxRMS)
+	{
+		// Plane fit too poor -> no attach
+		bIsAttached = false;
+		return;
+	}
+
+	//Compare plane normal with skeletal mesh local up (in world)
+	FVector MeshLocalUpWorld = SkeletalMeshComponent->GetComponentTransform().TransformVector(FVector(0.f, 0.f, 1.f));
+	MeshLocalUpWorld.Normalize();
+
+	float AlignDot = FVector::DotProduct(Normal, MeshLocalUpWorld);
+	// If dot negative, flip normal so it's oriented toward mesh up for a consistent comparison
+	if (AlignDot < 0.f)
+	{
+		Normal *= -1.f;
+		AlignDot = -AlignDot;
+	}
+
+	if (AlignDot < AlignDotThreshold)
+	{
+		// Surface too slanted compared to mesh up -> cannot attach
+		bIsAttached = false;
+		return;
+	}
+
+	// Apply rotation to skeletal mesh immediately to align mesh up to plane normal
+	FQuat DeltaQuat = FQuat::FindBetweenNormals(MeshLocalUpWorld, Normal);
+	// Apply to current component rotation
+	FTransform MeshTransform = SkeletalMeshComponent->GetComponentTransform();
+	FQuat CurrentQuat = MeshTransform.GetRotation();
+	FQuat orientedRotation = DeltaQuat * CurrentQuat;
+	targetRotation = orientedRotation;
+
+	// Second raycasts: from each default attach position straight down
+	// Compute reach using your expression
+	float Reach = FMath::Max(currentVelocity.Size() * lastDeltaTime * 2.f, 10.f);
+
+	FVector MeshLocalDownWorld = SkeletalMeshComponent->GetComponentTransform().
+	                                                    TransformVector(FVector(0.f, 0.f, -1.f));
+	MeshLocalDownWorld.Normalize();
+
+	TArray<FHitResult> legHits;
+	legHits.SetNumZeroed(legNames.Num());
+
+	FTransform orientedTransform = SkeletalMeshComponent->GetComponentTransform();
+	orientedTransform.SetRotation(orientedRotation);
+
+	for (int i = 0; i < legNames.Num(); ++i)
+	{
+		FVector LegLocal = defaultAttachPositions[i];
+		FVector LegWorldPos = orientedTransform.TransformPosition(LegLocal);
+
+		FVector RayStart = LegWorldPos - MeshLocalDownWorld * 200.f; // start a bit above
+		FVector RayEnd = LegWorldPos + MeshLocalDownWorld * Reach; // cast downwards
+
+		FHitResult LegHit;
+		bool bHit = GetWorld()->LineTraceSingleByChannel(LegHit, RayStart, RayEnd, ECollisionChannel::ECC_Visibility,
+		                                                 Params);
+
+		if (!bHit || LegHit.ImpactNormal.Z < MinLegSurfaceDot)
+		{
+			// One leg failed -> abort attaching
+			bIsAttached = false;
+			return;
+		}
+
+		legHits[i] = LegHit;
+	}
+
+	// Attach legs (use same rules as original)
+	for (int i = 0; i < legNames.Num(); ++i)
+	{
+		FHitResult& LegHit = legHits[i];
+		USceneComponent* AttachComp = AttachPoints[i];
+		if (!AttachComp || !LegHit.Component.IsValid())
+		{
+			bIsAttached = false;
+			return;
+		}
+		AttachComp->AttachToComponent(
+			LegHit.Component.Get(),
+			FAttachmentTransformRules(EAttachmentRule::KeepWorld, EAttachmentRule::KeepWorld,
+			                          EAttachmentRule::KeepWorld, false),
+			LegHit.BoneName);
+		AttachComp->SetWorldLocation(LegHit.ImpactPoint);
+	}
+
+
+	currentVelocity = FVector::ZeroVector;
+	bIsAttached = true;
+}
+
+
+void ADragon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bIsAttached)
+	{
+		// Gravity and motion logic unchanged
+		force += FVector(0, 0, -980) * DeltaTime;
+		currentVelocity += force;
+		force = FVector(0, 0, 0);
+
+		float verticalDamping = 0.3;
+		verticalDamping = FMath::Pow(1 - verticalDamping, DeltaTime);
+		float horizontalDamping = 0.5;
+		horizontalDamping = FMath::Pow(1 - horizontalDamping, DeltaTime);
+
+		currentVelocity.Y += horizontalDamping;
+		currentVelocity.X += horizontalDamping;
+		currentVelocity.Z *= verticalDamping;
+
+		FQuat currentRot = SkeletalMeshComponent->GetComponentQuat();
+		FQuat delta = targetRotation * currentRot.Inverse();
+		delta.Normalize();
+		FVector deltaAxis;
+		float deltaAngle;
+		delta.ToAxisAndAngle(deltaAxis, deltaAngle);
+		float rotationPerSecond = 0.9f;
+		float alpha = 1.0f - FMath::Pow(1.0f - rotationPerSecond, DeltaTime);
+		float takenAngle = alpha * deltaAngle;
+		FQuat stepQuat(deltaAxis, takenAngle);
+		FQuat newRot = stepQuat * currentRot;
+
+		SkeletalMeshComponent->SetWorldRotation(
+			newRot, false, nullptr, ETeleportType::TeleportPhysics
+		);
+
+
+		AddTranslation(currentVelocity * DeltaTime);
+
+		reattach();
+	}
+	else
+	{
+		if (AttachPoints.Num() == 4 && defaultAttachPositions.Num() == 4)
+		{
+			FVector sumDeltaTranslation = FVector::ZeroVector;
+			FQuat totalQuat = FQuat::Identity;
+
+			FVector selfLocationWorld = SkeletalMeshComponent->GetComponentLocation();
+
+			for (int i = 0; i < AttachPoints.Num(); ++i)
+			{
+				FVector attachPointWorld = AttachPoints[i]->GetComponentLocation();
+				FVector defaultPointWorld = SkeletalMeshComponent->GetComponentTransform().TransformPosition(defaultAttachPositions[i]);
+
+				// Convert to local space relative to SkeletalMesh location
+				FVector attachLocal = attachPointWorld - selfLocationWorld;
+				FVector defaultLocal = defaultPointWorld - selfLocationWorld;
+
+				sumDeltaTranslation += (attachPointWorld - defaultPointWorld);
+
+				FQuat q = FQuat::FindBetweenNormals(defaultLocal.GetSafeNormal(), attachLocal.GetSafeNormal());
+
+				// Don't add a point from the opposite hemisphere
+				float dot = totalQuat.X * q.X + totalQuat.Y * q.Y + totalQuat.Z * q.Z + totalQuat.W * q.W;
+				if (dot < 0)
+				{
+					q = q * -1.0f;
+				}
+
+				totalQuat.X += q.X;
+				totalQuat.Y += q.Y;
+				totalQuat.Z += q.Z;
+				totalQuat.W += q.W;
+			}
+
+			FVector deltaTranslation = sumDeltaTranslation / AttachPoints.Num();
+			totalQuat.Normalize();
+
+			FTransform newTransform = FTransform(totalQuat * SkeletalMeshComponent->GetComponentQuat(), selfLocationWorld + deltaTranslation);
+			SkeletalMeshComponent->SetWorldTransform(newTransform, false, nullptr, ETeleportType::TeleportPhysics);
+
+			// Verify the positions of the points
+			{
+
+				TArray<float> distances = {};
+				for (int i = 0; i < AttachPoints.Num(); i++)
+				{
+					FVector attachPointWorldPos = AttachPoints[i]->GetComponentLocation();
+					FVector defaultPointWorldPos = SkeletalMeshComponent->GetComponentTransform().TransformPosition(defaultAttachPositions[i]);
+					distances.Add(FVector::Dist(attachPointWorldPos, defaultPointWorldPos));
+				}
+				
+				float sumDistance = 0;
+				for (int i = 0; i < distances.Num(); i++)
+				{
+					sumDistance += distances[i];
+				}
+
+				if (sumDistance > 50)
+				{
+					reattach();
+				}
+			}
+		}
+	}
+	lastDeltaTime = DeltaTime;
+}
+
+
+void ADragon::followPlayer(float DeltaTime)
+{
+	ACharacter* player = Cast<ACharacter>(UGameplayStatics::GetActorOfClass(this, ACharacter::StaticClass()));
+
+	if (player)
+	{
+		FVector playerPos = player->GetActorLocation();
+		FVector FloorLocation = playerPos;
+		// Raycast floor
+		{
+			FVector Start = playerPos;
+			FVector End = Start - FVector(0, 0, 10000); // cast 10,000 units downward
+
+			FHitResult HitResult;
+			FCollisionQueryParams Params;
+			Params.AddIgnoredActor(player); // don't hit yourself
+
+			bool bHit = GetWorld()->LineTraceSingleByChannel(
+				HitResult,
+				Start,
+				End,
+				ECC_Visibility,
+				Params
+			);
+
+			if (bHit)
+			{
+				FloorLocation = HitResult.Location;
+			}
+		}
+
+		FVector direction = (FloorLocation - GetActorLocation());
+		if (direction.Length() > 150)
+		{
+			direction.Normalize();
+			FTransform newTransform = SkeletalMeshComponent->GetRelativeTransform();
+			newTransform.SetLocation(newTransform.GetLocation() + direction * DeltaTime * MovementSpeed);
+
+			FQuat forwardRotation = direction.Rotation().Quaternion();
+			FQuat Adjust(FRotator(0.f, -90.f, 0.f));
+			forwardRotation = Adjust * forwardRotation;
+
+			newTransform.SetRotation(forwardRotation);
+
+
+			SetActorTransform(newTransform, false, nullptr, ETeleportType::TeleportPhysics);
+			SkeletalMeshComponent->SetWorldTransform(newTransform);
+		}
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 60, FColor::Red, TEXT("Cannot find player"));
+	}
+}
---
+++ Source/GEngine3Aleksei/Enemy/Dragon.h
@@ -0,0 +1,227 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Animation/AnimInstance.h"
+#include "Dragon.generated.h"
+
+
+class UPhysicsControlComponent;
+UCLASS()
+
+class GENGINE3ALEKSEI_API UDragonAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FName> PhysicalBones;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FTransform> PhysicalTransforms;
+};
+
+
+class UCapsuleComponent;
+
+UCLASS()
+class GENGINE3ALEKSEI_API ADragon : public AActor
+{
+	GENERATED_BODY()
+	
+protected:
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    USkeletalMeshComponent* SkeletalMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPhysicsControlComponent* PhysicsControl;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere)
+	TArray<USceneComponent*> AttachPoints;
+	TArray<FVector> defaultAttachPositions; // Positions of legs in local coordinate system.
+	FVector defaultLegCenter;
+
+	
+	
+	FVector currentVelocity;
+	FVector force;
+	float lastDeltaTime;
+	
+	bool bIsAttached = false;
+	
+public:	
+	// Sets default values for this actor's properties
+	ADragon();
+
+	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void AddTranslation(FVector DeltaPos);
+	void SetTranslation(FVector Pos);
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void reattach();
+
+	TArray<FName> legNames = {TEXT("Bone_015"), TEXT("Bone_041"), TEXT("Bone_024"), TEXT("Bone_036")};
+
+	FQuat targetRotation = FQuat::Identity;
+ 	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	void followPlayer(float DeltaTime);
+
+
+	
+	static FQuat FindBestFitQuaternion(const TArray<FVector>& OriginalPoints, const TArray<FVector>& CurrentPoints, float& OutError)
+{
+    check(OriginalPoints.Num() == CurrentPoints.Num() && OriginalPoints.Num() >= 3);
+    const int32 N = OriginalPoints.Num();
+
+    // 1) compute centroids
+    FVector CentroidOrig(0,0,0), CentroidCurr(0,0,0);
+    for (int32 i = 0; i < N; ++i)
+    {
+        CentroidOrig += OriginalPoints[i];
+        CentroidCurr += CurrentPoints[i];
+    }
+    CentroidOrig /= (float)N;
+    CentroidCurr /= (float)N;
+
+    // 2) compute 3x3 covariance matrix (double precision for stability)
+    double Cov[3][3] = { {0.0,0.0,0.0}, {0.0,0.0,0.0}, {0.0,0.0,0.0} };
+    for (int32 i = 0; i < N; ++i)
+    {
+        const FVector P = OriginalPoints[i] - CentroidOrig;
+        const FVector Q = CurrentPoints[i] - CentroidCurr;
+        Cov[0][0] += (double)P.X * (double)Q.X;
+        Cov[0][1] += (double)P.X * (double)Q.Y;
+        Cov[0][2] += (double)P.X * (double)Q.Z;
+
+        Cov[1][0] += (double)P.Y * (double)Q.X;
+        Cov[1][1] += (double)P.Y * (double)Q.Y;
+        Cov[1][2] += (double)P.Y * (double)Q.Z;
+
+        Cov[2][0] += (double)P.Z * (double)Q.X;
+        Cov[2][1] += (double)P.Z * (double)Q.Y;
+        Cov[2][2] += (double)P.Z * (double)Q.Z;
+    }
+
+    // 3) Build 4x4 symmetric N matrix from Horn (using doubles)
+    // N = [ trace(Cov) , (Cov[1][2]-Cov[2][1]), (Cov[2][0]-Cov[0][2]), (Cov[0][1]-Cov[1][0]);
+    //       ... symmetric ... ]
+    double Nmat[4][4];
+    double trace = Cov[0][0] + Cov[1][1] + Cov[2][2];
+
+    Nmat[0][0] = trace;
+    Nmat[0][1] = Cov[1][2] - Cov[2][1];
+    Nmat[0][2] = Cov[2][0] - Cov[0][2];
+    Nmat[0][3] = Cov[0][1] - Cov[1][0];
+
+    Nmat[1][0] = Nmat[0][1];
+    Nmat[1][1] = Cov[0][0] - Cov[1][1] - Cov[2][2];
+    Nmat[1][2] = Cov[0][1] + Cov[1][0];
+    Nmat[1][3] = Cov[0][2] + Cov[2][0];
+
+    Nmat[2][0] = Nmat[0][2];
+    Nmat[2][1] = Nmat[1][2];
+    Nmat[2][2] = -Cov[0][0] + Cov[1][1] - Cov[2][2];
+    Nmat[2][3] = Cov[1][2] + Cov[2][1];
+
+    Nmat[3][0] = Nmat[0][3];
+    Nmat[3][1] = Nmat[1][3];
+    Nmat[3][2] = Nmat[2][3];
+    Nmat[3][3] = -Cov[0][0] - Cov[1][1] + Cov[2][2];
+
+    // 4) Power iteration to find largest eigenvector of 4x4 symmetric matrix.
+    // Use double for numerical stability.
+    double v[4] = {1.0, 0.0, 0.0, 0.0}; // initial guess (can be any non-zero)
+    const int MaxIters = 100;
+    const double Eps = 1e-12;
+    for (int iter = 0; iter < MaxIters; ++iter)
+    {
+        double w[4] = {0.0, 0.0, 0.0, 0.0};
+        // w = Nmat * v
+        for (int r = 0; r < 4; ++r)
+        {
+            double sum = 0.0;
+            for (int c = 0; c < 4; ++c)
+            {
+                sum += Nmat[r][c] * v[c];
+            }
+            w[r] = sum;
+        }
+
+        // normalize w -> next v
+        double norm = 0.0;
+        for (int i = 0; i < 4; ++i) norm += w[i] * w[i];
+        if (norm <= 0.0) break;
+        norm = FMath::Sqrt((float)norm);
+        for (int i = 0; i < 4; ++i) w[i] /= norm;
+
+        // check convergence: ||v - w||
+        double diff = 0.0;
+        for (int i = 0; i < 4; ++i)
+        {
+            double d = v[i] - w[i];
+            diff += d * d;
+            v[i] = w[i];
+        }
+        if (diff < Eps) break;
+    }
+
+    // Ensure quaternion ordering: Horn eigenvector is [q0, qx, qy, qz]
+    double q0 = v[0];
+    double qx = v[1];
+    double qy = v[2];
+    double qz = v[3];
+
+    // If vector is near zero for some reason, fallback to identity
+    double vnorm2 = q0*q0 + qx*qx + qy*qy + qz*qz;
+    if (vnorm2 < 1e-16)
+    {
+        OutError = 0.0f;
+        return FQuat::Identity;
+    }
+
+    // Normalize quaternion (convert to floats for FQuat)
+    double invNorm = 1.0 / FMath::Sqrt((float)vnorm2);
+    q0 *= invNorm;
+    qx *= invNorm;
+    qy *= invNorm;
+    qz *= invNorm;
+
+    // Unreal's FQuat constructor is FQuat(X, Y, Z, W) where W is scalar part.
+    FQuat Rotation((float)qx, (float)qy, (float)qz, (float)q0);
+    Rotation.Normalize();
+
+    // 5) Compute RMSD (error)
+    double sumSq = 0.0;
+    for (int32 i = 0; i < N; ++i)
+    {
+        const FVector Pcenter = OriginalPoints[i] - CentroidOrig;
+        const FVector Transformed = Rotation.RotateVector(Pcenter) + CentroidCurr;
+        const double dx = (double)Transformed.X - (double)CurrentPoints[i].X;
+        const double dy = (double)Transformed.Y - (double)CurrentPoints[i].Y;
+        const double dz = (double)Transformed.Z - (double)CurrentPoints[i].Z;
+        sumSq += dx*dx + dy*dy + dz*dz;
+    }
+    OutError = FMath::Sqrt((float)(sumSq / (double)N));
+
+    return Rotation;
+}
+
+
+
+
+};
